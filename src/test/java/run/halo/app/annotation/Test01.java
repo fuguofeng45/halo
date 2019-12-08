1c070fd41c5024569352482616d475ca86a46d92
fuguofeng45 <15524275040@163.com>
2019-12-08 21:43:42 +0800

    jdk1.8测试

--- src/test/java/run/halo/app/annotation/Test01.java
+++ src/test/java/run/halo/app/annotation/Test01.java
@@ -1,4 +1,17 @@
 package run.halo.app.annotation;
 
+import org.springframework.lang.NonNull;
+
+import javax.validation.constraints.NotNull;
+
 public class Test01 {
+
+    static String getTestString(@NonNull String string){
+        return string;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getTestString(null));
+    }
+
 }
