6438ff9353376d29d2204cc909076173552d9a7f
fuguofeng45 <15524275040@163.com>
2019-12-29 20:57:37 +0800

    swagger配置

--- src/main/java/run/halo/app/config/SwaggerConfiguration.java
+++ src/main/java/run/halo/app/config/SwaggerConfiguration.java
@@ -19,6 +19,7 @@ import run.halo.app.security.support.UserDetail;
 import springfox.documentation.builders.*;
 import springfox.documentation.schema.AlternateTypeRule;
 import springfox.documentation.schema.AlternateTypeRuleConvention;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -29,6 +30,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.lang.reflect.Type;
 import java.time.temporal.Temporal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -125,6 +127,7 @@ public class SwaggerConfiguration {
         return Arrays.asList(
                 new ApiKey("Token from header", AdminAuthenticationFilter.ADMIN_TOKEN_HEADER_NAME, In.HEADER.name()),
                 new ApiKey("Token from query", AdminAuthenticationFilter.ADMIN_TOKEN_QUERY_NAME, In.QUERY.name())
+                //new ApiKey("Authorization", "Authorization", "header")
         );
     }
 
